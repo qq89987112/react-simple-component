a8ae6b702b6a83a2e80abf125c515a3fd8f87a11
蔡荣庚 <89987112@qq.com>
2017-12-28 10:50:07 +0800

    update BaseComponent

--- BaseComponent.js
+++ BaseComponent.js
@@ -1,10 +1,27 @@
-
 import React from "react";
 
 class BaseComponent extends React.Component{
+
+    constructor(){
+        super();
+        this.onInput = (name)=>{
+            return (event)=>{
+                const target = event.target;
+                const value = target.type === 'checkbox' ? target.checked : target.value;
+                // const name = target.name;
+                this.setState({
+                    [name]: value
+                });
+            }
+
+        }
+    }
+
     $toast(){
         console.log('$toast');
     }
+
+
 }
 
 export default BaseComponent;
