126b9ee9ba29dee15f79164dae258d411f5d3ff8
蔡荣庚 <89987112@qq.com>
2018-01-02 10:50:51 +0800

    'addload'

--- BaseComponent.js
+++ BaseComponent.js
@@ -3,6 +3,7 @@ import { Modal } from 'antd';
 
 class BaseComponent extends React.Component{
 
+
     $onInput = (name)=>{
         return (event)=>{
             const target = event.target;
@@ -19,7 +20,7 @@ class BaseComponent extends React.Component{
         // //暂时使用log
         // console.log(msg);
 
-        const modal = Modal.success({
+        Modal.success({
             content: msg,
         });
         // setTimeout(() => modal.destroy(), 1000);
@@ -30,14 +31,36 @@ class BaseComponent extends React.Component{
         // //暂时使用error
         // console.error(msg);
 
-        const modal = Modal.error({
+        Modal.error({
             content: msg,
         });
         // setTimeout(() => modal.destroy(), 1000);
     }
 
 
+    $load = (name)=>{
+        let loadings = this.state.loadings || new Set();
+        loadings.add(name);
+        this.setState({
+            loadings:loadings
+        })
+    }
 
+    $cancel(name){
+        let loadings = this.state.loadings || new Set();
+        loadings.delete(name);
+        this.setState({
+            loadings:loadings
+        })
+    }
+
+    // 可以用来做 ant button 的 loading 绑定
+    // <Button type='primary' loading={this.$isLoading('submitForget')}>提交</Button>
+
+    $isLoading(name){
+        let loadings = this.state.loadings || new Set();
+        return loadings.has(name);
+    }
 
 }
 
