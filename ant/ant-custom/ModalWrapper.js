82c90c1c30ccdca08150f2593df99cce31ee7cd8
蔡荣庚 <89987112@qq.com>
2018-01-08 10:19:58 +0800

    update modalWrapper

--- ant/ant-custom/ModalWrapper.js
+++ ant/ant-custom/ModalWrapper.js
@@ -10,105 +10,109 @@ class ModalWrapper extends BaseAntPage {
 
     static instance;
 
-    static $new = () => {
+    static $new = (props = {footer: null}) => {
+    let
+    div = document.createElement('div'),
+    instance;
+
+    document.body.appendChild(div);
+    instance = ReactDOM.render(React.createElement(ModalWrapper, props), div);
+    instance.__div__ = div;
+    return instance;
+}
+
+static $getInstance = (props) => {
+    if (!ModalWrapper.instance) {
+        ModalWrapper.instance = ModalWrapper.$new(props);
+    }
+    return ModalWrapper.instance;
+}
+
+static $close = (...params) => {
+    return ModalWrapper.$getInstance().close(...params);
+}
+
+static $showNew = (reactNodeFunc,props) => {
+    let
+        $new = ModalWrapper.$new(props),
+        _close = $new.close;
+    $new.close = ()=>_close({destory:true});
+    return $new.show(reactNodeFunc);
+}
+
+static $show = (reactNodeFunc,props) => {
+    return ModalWrapper.$getInstance(props).show(reactNodeFunc);
+}
+
+show = (reactNodeFunc) => {
+    return new Promise((resolve, reject) => {
+        // 不能保存函数,否则 const orderList = apiUtils.wrapWidthLoading(warehouse.orderList, 'orderList', 'orderList'); 不能在函数里使用,因为render里不能操作state
+        // this.modalContentFun = () => reactNodeFunc(....);
+        // 如果使用非函数的话,则对instance的setState就没有效果(拿的缓存)。
         let
-            div = document.createElement('div'),
-            instance;
+            type = Object.prototype.toString.call(reactNodeFunc),
+        render,
+        onShow,
+        params = {
+            resolve,
+            reject,
+            //params, // showModal时传过来的。 reactNode就跟在show后面.....
+            // form: {},  // 供modal里的表单使用,方便resolve result,而不是把相关变量放在父组件中。
+            instance: this // from 可以合并到这里
+        };;
 
-        document.body.appendChild(div);
-        instance = ReactDOM.render(React.createElement(ModalWrapper, {footer: null}), div);
-        instance.__div__ = div;
-        return instance;
+    if(type==='[object Function]'){
+        render = reactNodeFunc;
+        onShow = ()=>{};
+    }else if(type==='[object Object]'){
+        render = reactNodeFunc.render;
+        onShow = reactNodeFunc.onShow || (()=>{});
     }
 
-    static $getInstance = () => {
-        if (!ModalWrapper.instance) {
-            ModalWrapper.instance = ModalWrapper.$new();
-        }
-        return ModalWrapper.instance;
-    }
-
-    static $close = (...params) => {
-        return ModalWrapper.$getInstance().close(...params);
-    }
-
-    static $showNew = (...params) => {
-        return ModalWrapper.$new().show(...params);
-    }
-
-    static $show = (...params) => {
-        return ModalWrapper.$getInstance().show(...params);
-    }
-
-    show = (reactNodeFunc) => {
-        return new Promise((resolve, reject) => {
-            // 不能保存函数,否则 const orderList = apiUtils.wrapWidthLoading(warehouse.orderList, 'orderList', 'orderList'); 不能在函数里使用,因为render里不能操作state
-            // this.modalContentFun = () => reactNodeFunc(....);
-            // 如果使用非函数的话,则对instance的setState就没有效果(拿的缓存)。
-            let
-                type = Object.prototype.toString.call(reactNodeFunc),
-                render,
-                onShow,
-                params = {
-                    resolve,
-                    reject,
-                    //params, // showModal时传过来的。 reactNode就跟在show后面.....
-                    // form: {},  // 供modal里的表单使用,方便resolve result,而不是把相关变量放在父组件中。
-                    instance: this // from 可以合并到这里
-                };;
-
-                if(type==='[object Function]'){
-                    render = reactNodeFunc;
-                    onShow = ()=>{};
-                }else if(type==='[object Object]'){
-                    render = reactNodeFunc.render;
-                    onShow = reactNodeFunc.onShow || (()=>{});
-                }
 
 
+    onShow(params);
+    this.contentRender = ()=>render(params)
+    this.setState({
+        visible: true
+    })
+});
+}
 
-            onShow(params);
-            this.contentRender = ()=>render(params)
-            this.setState({
-                visible: true
-            })
-        });
-    }
+contentRender = () => {}
 
-    contentRender = () => {}
+close = ({destory = false, clear = true} = {}) => {
+    this.setState({
+        visible: false
+    });
+    clear && (this.contentRender = () => {
+    });
 
-    close = ({destory = false, clear = true} = {}) => {
-        this.setState({
-            visible: false
-        });
-        clear && (this.contentRender = () => {
-        });
+    if (destory) {
+        let div = this.__div__;
 
-        if (destory) {
-            let div = this.__div__;
-
-            const unmountResult = ReactDOM.unmountComponentAtNode(div);
-            if (unmountResult && div.parentNode) {
-                div.parentNode.removeChild(div);
-                if (ModalWrapper.instance === this) {
-                    ModalWrapper.instance = null;
-                }
+        const unmountResult = ReactDOM.unmountComponentAtNode(div);
+        if (unmountResult && div.parentNode) {
+            div.parentNode.removeChild(div);
+            if (ModalWrapper.instance === this) {
+                ModalWrapper.instance = null;
             }
         }
     }
+}
 
 
-    render() {
-        return (
-            <Modal
-                {...this.props}
-                onCancel={this.close}
-                visible={this.state.visible}
-            >
-                {this.contentRender()}
-            </Modal>
-        )
-    }
+render() {
+    return (
+        <Modal
+    {...this.props}
+    onCancel={this.close}
+    visible={this.state.visible}
+>
+    {this.contentRender()}
+</Modal>
+)
+}
 
 }
 
