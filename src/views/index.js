cbf749b4f66a4f54781ed00f46df04bcbcf33024
蔡荣庚 <89987112@qq.com>
2018-01-31 00:02:27 +0800

    udpate

--- src/views/index.js
+++ src/views/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import BaseComponent from "../components/BaseComponent";
-import { HashRouter,Route,Link } from 'react-router-dom'
+import { HashRouter,Route,Link,Redirect } from 'react-router-dom'
 import 'moment/locale/zh-cn';
 import Home from "./Home";
 import Login from "./Login";
@@ -10,7 +10,8 @@ class Router extends BaseComponent{
     render(){
         return (<HashRouter>
             <div>
-                <Route exact path="/" component={Login}/>
+                <Redirect to='/login'/>
+                <Route exact path="/login" component={Login}/>
                 <Route exact path="/home" component={Home}/>
             </div>
 
