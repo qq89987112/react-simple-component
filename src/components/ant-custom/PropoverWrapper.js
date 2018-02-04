34a577f5736e198151eb719c4acdc6dee4e2aac9
蔡荣庚 <89987112@qq.com>
2018-02-04 14:05:35 +0800

    update

---
+++ src/components/ant-custom/PropoverWrapper.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Popover,Form,Input,Button,Select} from 'antd'
+import BaseComponent from "../../../components/Base/BaseComponent";
+
+
+export default class InputPropover extends BaseComponent {
+
+    close(){
+        this.setState({
+            popover:false
+        })
+    }
+
+    render() {
+        let { title,children,content} = this.props;
+
+        content = content({instance:this});
+
+        return (
+            <Popover placement="top"  onVisibleChange={v=> this.setState({popover:v})} visible={this.state.popover} title={title} content={content} trigger="click">
+                <Button>{children}</Button>
+            </Popover>
+        )
+    }
+}
