bb7950ef4176226e154cd323356621eacb5da2bc
蔡荣庚 <89987112@qq.com>
2018-02-25 19:54:12 +0800

    update

--- src/components/TableComponent.js
+++
@@ -1,8 +0,0 @@
-import BaseComponent from "./BaseComponent";
-import TableUtils from "./ant-custom/TableUtils";
-
-class TableComponent extends BaseComponent {
-    tableUtils = new TableUtils(this);
-}
-
-export default TableComponent;
