3f780eb98374b7999e6fcdb3f27d28286e96f047
蔡荣庚 <89987112@qq.com>
2018-03-10 22:38:01 +0800

    update

---
+++ src/components/Confirm.js
@@ -0,0 +1,14 @@
+import {Popconfirm} from "antd";
+import React from "react";
+import BaseComponent from "./BaseComponent";
+
+
+
+export default class Confirm extends BaseComponent {
+    render(){
+        const {title="确定要删除？",children,...rest} = this.props;
+        return  <Popconfirm title={title} okText="是" cancelText="否" {...rest}>
+            {children||"删除"}
+        </Popconfirm>
+    }
+};
