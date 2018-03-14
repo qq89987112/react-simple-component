8e95267af8eb0127a8fff2fb63c9ef3ddb2d4e27
蔡荣庚 <89987112@qq.com>
2018-03-14 23:12:51 +0800

    update

--- src/components/Confirm.js
+++ src/components/Confirm.js
@@ -6,6 +6,7 @@ import BaseComponent from "./BaseComponent";
 
 export default class Confirm extends BaseComponent {
     render(){
+        //onConfirm onCancel
         const {title="确定要删除？",children,...rest} = this.props;
         return  <Popconfirm title={title} okText="是" cancelText="否" {...rest}>
             {children||"删除"}
