e89852794418dcfebdbbc8f6f6d050f8ba5dd828
蔡荣庚 <89987112@qq.com>
2018-03-17 19:19:55 +0800

    update

--- src/components/Confirm.js
+++ src/components/Confirm.js
@@ -7,8 +7,20 @@ import BaseComponent from "./BaseComponent";
 export default class Confirm extends BaseComponent {
     render(){
         //onConfirm onCancel
-        const {title="确定要删除？",children,...rest} = this.props;
-        return  <Popconfirm title={title} okText="是" cancelText="否" {...rest}>
+        const {title="确定要删除？",children,jump=false,...rest} = this.props;
+        return  <Popconfirm
+            visible={this.state.visible}
+            onVisibleChange={v=>{
+                if (jump&&v) {
+                    let onConfirm = rest.onConfirm;
+                    onConfirm&&onConfirm();
+                }else{
+                    this.setState({ visible:v });
+                }
+            }}
+            title={title}
+            okText="是"
+            cancelText="否" {...rest}>
             {children||"删除"}
         </Popconfirm>
     }
