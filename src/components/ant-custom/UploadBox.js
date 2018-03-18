27a7c8c99f2f0553d7ca097384821988499570bc
蔡荣庚 <89987112@qq.com>
2018-03-18 12:53:01 +0800

    update

--- src/components/ant-custom/UploadBox.js
+++ src/components/ant-custom/UploadBox.js
@@ -1,42 +1,72 @@
-import React from 'react';
-import { Upload, Icon, message } from 'antd';
-import BaseComponent from "./ant-custom/BaseAntPage";
-const Dragger = Upload.Dragger;
+import React from "react"
+import {Upload, Icon, message} from 'antd';
+import axios from "axios"
 
 
-
-class UploadBox extends BaseComponent {
-
-    render() {
-        const {onResult,url} = this.props;
-        const props = {
-            name: 'file',
-            multiple: true,
-            action: url,
-            showUploadList:false,
-            onChange(info) {
-                const status = info.file.status;
-                if (status !== 'uploading') {
-                    console.log(info.file, info.fileList);
-                }
-                if (status === 'done') {
-                    // message.success(`${info.file.name} file uploaded successfully.`);
-                    onResult&&onResult(info,info.file.response);
-                } else if (status === 'error') {
-                    message.error(`${info.file.name} file upload failed.`);
-                }
-            },
-        };
-
-        return (
-            <Dragger {...props}>
-                <p className="ant-upload-drag-icon">
-                    <Icon type="inbox" />
-                </p>
-                <p className="ant-upload-text">点击或拖拽文件到这个区域</p>
-            </Dragger>
-        )
-    }
+function beforeUpload(file) {
+    return true;
 }
 
-export default UploadBox
+export default class UploadBox extends React.Component {
+    state = {
+        loading: false,
+    };
+    handleChange = (info) => {
+        let file = info.file;
+        if (file.status === 'uploading') {
+            this.setState({loading: true});
+            return;
+        }
+        if (file.status === 'done') {
+            const data = file.response;
+            const {onInput} = this.props;
+            let fileUrl = `${axios.defaults.baseURL || ""}${data.data.fileUrl}`;
+            onInput && onInput(fileUrl);
+            this.setState({
+                url: fileUrl,
+                loading:false
+            })
+        }
+    }
+
+    render() {
+        const {url} = this.state;
+        const {defaultUrl,type = 'image'} = this.props;
+        const
+            style = {
+                border: "1px solid #e4e4e4",
+                width: 100,
+                lineHeight: "100px",
+                display: 'inline-block',
+                height: "100px",
+            };
+
+        let viewUrl = url || defaultUrl; //用于显示的视图url
+        return (
+            <Upload
+                name="file"
+                listType="picture-card"
+                className="avatar-uploader"
+                showUploadList={false}
+                action={`${axios.defaults.baseURL || ""}/api/base/UploadFile`}
+                beforeUpload={beforeUpload}
+                onChange={this.handleChange}
+            >
+                {
+                    this.state.loading&&<Icon type='loading'/>||
+                    // 如果有视图URL，就显示
+                    viewUrl && (
+                        <div style={style} target="_blank" href={viewUrl}>
+                            {
+                                this.state.loading && <Icon style={style} type='loading'/> ||
+                                type === 'image' && <img style={style} src={viewUrl} alt=""/> ||
+                                <Icon style={style} type="file"/>
+                            }
+                        </div>) ||
+                    //没有就上传
+                    <Icon type='plus'/>
+                }
+            </Upload>
+        );
+    }
+}
