4f12b0704f2943b66945037a60daa4c00a9744b6
蔡荣庚 <89987112@qq.com>
2018-03-29 14:48:24 +0800

    update

--- src/components/ant-custom/DynamicList/DynamicList.js
+++ src/components/ant-custom/DynamicList/DynamicList.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import {Select,Button,Icon} from 'antd'
 import "./DynamicList.scss"
+import BaseComponent from "../ant-custom/BaseAntPage";
 
 const
     Option = Select.Option;
 
 
-
-export default class DynamicList extends React.Component {
+//<DynamicList
+//    defaultList={list}
+//    renderItem={(item,index)=>{
+//        return <Input key={index} defaultValue={item.title} onInput={this.$onInput(v=>item.title=v)} />
+//    }}
+//    renderAdd={(instance)=>
+//        <p className='tac'>
+//            <Button onClick={instance.add}>{this.$f('add')}</Button>
+//            <Button onClick={()=>{
+//                this.$load('submit');
+//                onSubmit&&onSubmit({
+//                    title:instance.state.list.map(i=>i.title)
+//                }).then(()=>{
+//                    this.$cancel('submit')
+//                });
+//            }}>{this.$f('submit')}</Button>
+//        </p>}
+///>
+export default class DynamicList extends BaseComponent {
 
     state = {
         list:[]
@@ -54,7 +72,7 @@ export default class DynamicList extends React.Component {
                 }
                 <div class="footer">
                     {
-                        renderAdd(this) || <p style={{textAlign:'center'}}><Button type='primary' onClick={this.add}>添加</Button></p>
+                        renderAdd(this) || <p style={{textAlign:'center'}}><Button type='primary' onClick={this.add}>{this.$f('add')}</Button></p>
                     }
                 </div>
             </div>
--- src/components/ant-custom/SelectWrapper/SelectWrapper.js
+++ src/components/ant-custom/SelectWrapper/SelectWrapper.js
@@ -1,28 +1,27 @@
 import React from 'react';
-import {Select,Input} from 'antd'
+import {Select, Input} from 'antd'
 import BaseComponent from "./BaseAntPage";
 
 const
     Option = Select.Option;
 
 
-
 export default class SelectWrapper extends BaseComponent {
 
     state = {
-        list:undefined
+        list: undefined
     }
 
-    componentWillMount(){
+    componentWillMount() {
         //判断是否有dataSource 和 request 来选择性loaded
-        let { request,defaultValue,keyIndex } = this.props;
-        request&&request().then(list=>{
-            if(defaultValue&&keyIndex){
-                const value = list.find(item=>item[keyIndex]===defaultValue);
+        let {request, defaultValue, keyIndex} = this.props;
+        request && request().then(list => {
+            if (defaultValue && keyIndex) {
+                const value = list.find(item => item[keyIndex] === defaultValue);
                 defaultValue = list.indexOf(value);
                 // +1是因为多了一个unset选项
-                defaultValue = defaultValue===-1 ? undefined :defaultValue+1;
-            }else{
+                defaultValue = defaultValue === -1 ? undefined : defaultValue + 1;
+            } else {
                 defaultValue = undefined
             }
 
@@ -30,37 +29,37 @@ export default class SelectWrapper extends BaseComponent {
             this.setState({
                 list,
                 defaultValue,
-                loaded:true
+                loaded: true
             })
         })
     }
 
-    onChange = (v)=>{
+    onChange = (v) => {
         // 为了避免自己触发的改变还会再次在render里触发。
         this.lastValue = v;
         let {onChange} = this.props;
-        onChange&&onChange(this.getData()[v]);
+        onChange && onChange(this.getData()[v]);
     }
 
-    getData = ()=>{
-        let { dataSource=[],placeholder,dataIndex } = this.props;
-        const { list } = this.state;
+    getData = () => {
+        let {dataSource = [], placeholder, dataIndex} = this.props;
+        const {list} = this.state;
         dataSource = list || dataSource || [];
         // return [{[dataIndex]:placeholder||""},...dataSource];
-        return [{[dataIndex]:this.$f("nothing")},...dataSource];
+        return [{[dataIndex]: this.$f("nothing")}, ...dataSource];
     }
 
 
     render() {
-        let { dataIndex,defaultValue,onChange,value,keyIndex,...rest } = this.props;
-        let {loaded,list} = this.state;
+        let {dataIndex, defaultValue, onChange, value, keyIndex, ...rest} = this.props;
+        let {loaded, list} = this.state;
         let dataSource = this.getData();
 
         if (value && keyIndex) {
-            const tempValue = list.find(item=>item[keyIndex]===value);
+            const tempValue = list.find(item => item[keyIndex] === value);
             value = list.indexOf(tempValue);
             // +1是因为多了一个unset选项
-            value = value===-1 ? undefined :value+1;
+            value = value === -1 ? undefined : value + 1;
             //为了监听外部对value变量的更新，及时触发改变。
             if (value !== this.lastValue) {
                 this.onChange(value);
@@ -73,9 +72,11 @@ export default class SelectWrapper extends BaseComponent {
 
         return (
             <span>{
-                loaded&&<Select defaultValue={this.state.defaultValue} style={{ width: 120 }} onChange={this.onChange} {...{...value,...rest}}>
+                loaded && <Select defaultValue={this.state.defaultValue} style={{width: 120}}
+                                  onChange={this.onChange} {...{...value, ...rest}}>
                     {
-                        dataSource.map((item,index)=><Option title={item[dataIndex]} key={index} value={index}>{item[dataIndex]}</Option>)
+                        dataSource.map((item, index) => <Option title={item[dataIndex]} key={index}
+                                                                value={index}>{item[dataIndex]}</Option>)
                     }
                 </Select>
             }</span>
